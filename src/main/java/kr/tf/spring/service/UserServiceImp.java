ef60cc4f5258764566bdab486ee152bfd91baf1c
cocoparoma <bnbz201@naver.com>
2024-08-27 17:56:36 +0900

    암호화 세팅

--- src/main/java/kr/tf/spring/service/UserServiceImp.java
+++ src/main/java/kr/tf/spring/service/UserServiceImp.java
@@ -1,6 +1,7 @@
 package kr.tf.spring.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import kr.tf.spring.dao.UserDAO;
@@ -8,6 +9,13 @@ import kr.tf.spring.dao.UserDAO;
 @Service
 public class UserServiceImp implements UserService{
 	
+	// Dao 세팅
 	@Autowired
 	private UserDAO userDao;
+	
+	//암호화
+	@Autowired
+	BCryptPasswordEncoder passwordEncoder;
+
+
 }
