6c86290f29b9d567bd9c09774dfb4e28c10210b1
sujeelee <sujee54@gmail.com>
2024-09-11 17:41:58 +0900

    매장등록 1차 완료건

--- src/main/java/kr/tf/spring/model/vo/RestaurantVO.java
+++ src/main/java/kr/tf/spring/model/vo/RestaurantVO.java
@@ -7,12 +7,16 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class RestaurantVO {
 	private int re_id;
+	private String re_us_id;
 	private String re_x;
 	private String re_y;
 	private String re_name;
+	private int re_zip;
 	private String re_address;
+	private String re_address2;
 	private String re_phone;
 	private String re_tag;
+	private String re_category;
 	private String re_content;
 	private double re_score;
 }
