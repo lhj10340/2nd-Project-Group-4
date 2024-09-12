1f41fd7b1cdfbd3b31682da9d5ae1c84affb823d
sujeelee <sujee54@gmail.com>
2024-09-12 09:08:04 +0900

    매장등록2차 시작

--- src/main/java/kr/tf/spring/service/RestaurantServiceImp.java
+++ src/main/java/kr/tf/spring/service/RestaurantServiceImp.java
@@ -2,17 +2,24 @@ package kr.tf.spring.service;
 
 import java.util.List;
 
+import javax.annotation.Resource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import kr.tf.spring.dao.RestaurantDAO;
+import kr.tf.spring.model.vo.FileVO;
 import kr.tf.spring.model.vo.RestaurantVO;
+import kr.tf.spring.utils.UploadFileUtils;
 
 @Service
 public class RestaurantServiceImp implements RestaurantService {
 	@Autowired
 	private RestaurantDAO restaurantDao;
+	
+	@Resource
+	String uploadPath;
 
 	@Override
 	public RestaurantVO infoRest(String lat, String lng) {
@@ -47,9 +54,38 @@ public class RestaurantServiceImp implements RestaurantService {
 		if(rest == null) return false;
 		
 		boolean res = restaurantDao.restaurantRegistert(rest);
-		/*if(fileList == null || fileList.length == 0) {
+		if(fileList == null || fileList.length == 0) {
 			return true;
-		}*/
+		}
+		
+		if(!res) {
+			return false;
+		}
+		
+		//첨부파일 추가
+		for(MultipartFile file : fileList) {
+			uploadFile(file, rest.getRe_id());
+		}
 		return res;
+	}
+	
+	private void uploadFile(MultipartFile file, int re_id) {
+
+		if(file == null || file.getOriginalFilename().length() == 0) {
+			return;
+		}
+		
+		try {
+			String fi_ori_name = file.getOriginalFilename();
+			//첨부파일을 서버에 업로드 후 경로가 포함된 파일명을 가져옴
+			String fi_name = 
+			UploadFileUtils.uploadFile(uploadPath, fi_ori_name, file.getBytes());
+			//DB에 첨부파일 정보를 추가
+			FileVO fileVo = new FileVO(fi_name, fi_ori_name, re_id);
+			restaurantDao.insertFile(fileVo);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 	}
 }
