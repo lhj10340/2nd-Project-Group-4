bd59b8717397deb2d9bd685f8a9599dd7670559d
wlgns647 <wlgns64711234@gmail.com>
2024-09-13 01:00:47 +0900

    관리자권한으로 리뷰삭제가 가능하게 수정

--- src/main/java/kr/tf/spring/dao/ReviewDAO.java
+++ src/main/java/kr/tf/spring/dao/ReviewDAO.java
@@ -10,22 +10,36 @@ import kr.tf.spring.pagination.ReviewCriteria;
 
 public interface ReviewDAO {
 
-	List<ReviewVO> selectCommunityList();
-	
-	List<ReviewVO> selectReviewList(@Param("cri")ReviewCriteria cri);
+    // 모든 커뮤니티 리뷰 리스트 조회
+    List<ReviewVO> selectCommunityList();
 
-	int selectReviewTotalCount(@Param("cri")ReviewCriteria cri);
+    // 필터링된 리뷰 목록 조회
+    List<ReviewVO> selectReviewList(@Param("cri") ReviewCriteria cri);
 
-	boolean insertReview(@Param("review")ReviewVO review);
+    // 필터링된 리뷰 총 개수 조회
+    int selectReviewTotalCount(@Param("cri") ReviewCriteria cri);
 
-	void insertImage(@Param("image")ImageVO imageVO);
+    // 리뷰 추가
+    boolean insertReview(@Param("review") ReviewVO review);
 
-	ReviewVO selectReview(@Param("rv_id")Integer rv_id);
+    // 이미지 추가
+    void insertImage(@Param("image") ImageVO imageVO);
 
-	List<ImageVO> selectImageList(@Param("rv_id")Integer rv_id);
+    // 리뷰 조회
+    ReviewVO selectReview(@Param("rv_id") Integer rv_id);
 
-	List<ReviewVO> getAllReviews(@Param("cri")ReviewCriteria cri);
+    // 리뷰의 이미지 목록 조회
+    List<ImageVO> selectImageList(@Param("rv_id") Integer rv_id);
 
-	int selectTotalReviewCount();
+    // 모든 리뷰 조회 (페이지네이션 포함)
+    List<ReviewVO> getAllReviews(@Param("cri") ReviewCriteria cri);
 
+    // 모든 리뷰 총 개수 조회
+    int selectTotalReviewCount();
+
+    // 리뷰 삭제
+    int deleteReview(@Param("rv_id") int rv_id);
+
+    // 리뷰에 연관된 모든 이미지 삭제
+    int deleteImagesByReviewId(@Param("rv_id") int rv_id);
 }
