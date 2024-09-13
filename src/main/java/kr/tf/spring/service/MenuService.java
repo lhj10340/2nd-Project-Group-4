8e9deb010ea0a03360061b07ba4854502ded7a3a
sujeelee <sujee54@gmail.com>
2024-09-13 09:28:18 +0900

    Update MenuService.java

--- src/main/java/kr/tf/spring/service/MenuService.java
+++ src/main/java/kr/tf/spring/service/MenuService.java
@@ -2,6 +2,8 @@ package kr.tf.spring.service;
 
 import java.util.List;
 
+import org.springframework.web.multipart.MultipartFile;
+
 import kr.tf.spring.model.vo.MenuVO;
 import kr.tf.spring.model.vo.RestaurantVO;
 import kr.tf.spring.model.vo.UserVO;
